4332aeada27160c7a36cdf362cc5ff2c8213da42
NZHawk <nzhawknz@gmail.com>
2010-11-18 19:26:27 -0500

    few updates

--- pggl.h
+++
@@ -1,8 +0,0 @@
-/*
-  Header file for pggl.c
-*/
-
-void init_pggl();
-void draw(char input[], int posx, int posy);
-void drawInt(int number, int posx, int posy);
-void drawButton(PngDatas buttonDown, PngDatas buttonUp, int state, int posx, int poy);
